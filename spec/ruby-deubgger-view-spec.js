3e626a171dae469828a58e468583558bb41e077d
kyatul <atul.21j@gmail.com>
2016-04-21 14:20:01 +0530

    Create dummy package

---
+++ spec/ruby-deubgger-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RubyDeubggerView from '../lib/ruby-deubgger-view';
+
+describe('RubyDeubggerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
